f74a2531043dd3390ea246695d84c5e7819ac2e7
Lac <laclys@126.com>
2019-10-21 15:02:47 +0800

    feat: handle timeout error

--- src/types/index.ts
+++ src/types/index.ts
@@ -10,9 +10,10 @@ export interface AxiosRequestConfig {
   url: string
   method?: Method
   data?: any
-  params?: any,
+  params?: any
   headers?: any
   responseType?: XMLHttpRequestResponseType
+  timeout?: number
 }
 
 export interface AxiosResponse {
