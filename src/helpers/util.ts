81e0289b2df56bb2b29c3f1cdd7d227983a7be23
Lac <laclys@126.com>
2019-10-27 14:22:09 +0800

    refactor Axios.ts

--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -16,7 +16,10 @@ export function isPlainObject (val:any) : val is Object {
 
 export function extend<T, U>(to: T, from: U): T & U {
   for (const key in from) {
-    (to as T & U)[key] = from[key] as any
+    ;(to as T & U)[key] = from[key] as any
   }
   return to as T & U
 }
+
+
+// tips: 如果括号开头的语句不加分号，那么代码压缩后合并到一行后非常容易变成一个函数的调用了，所以需要加分号。另外以 +、-、/、()、[] 这些字符开头的语句，都需要加分号。
