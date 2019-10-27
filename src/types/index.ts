1ea590550656e9be4b82b87a0413ebf74dbd9ce8
Lac <laclys@126.com>
2019-10-27 18:36:34 +0800

    add InterceptorManager Class

--- src/types/index.ts
+++ src/types/index.ts
@@ -60,3 +60,17 @@ export interface AxiosInstance extends Axios {
 
   <T=any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
 }
+
+export interface AxiosInterceptorManager<T> {
+  use (resolved: ResolveFn<T>, rejected: RejectedFn): number
+
+  eject(id: number): void
+}
+
+export interface ResolveFn<T> {
+  (val: T):T | Promise<T>
+}
+
+export interface RejectedFn {
+  (error: any): any
+}
